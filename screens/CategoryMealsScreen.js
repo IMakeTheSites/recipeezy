19c1c06a040757e5c69b642c205aa00c137fc1d5
Mark Wlodawski <markwlodawski@marks-mbp-2.lan>
2020-12-19 22:22:06 -0500

    Initial commit, just added React-Navigation and it works between screens

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const CategoryMealsScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meals Screen!</Text>
+      <Button
+        title="Meal Details Screen!"
+        onPress={() => {
+          props.navigation.navigate("MealDetail");
+        }}
+      />
+      <Button
+        title="Go Back"
+        onPress={() => {
+          props.navigation.goBack();
+        }}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default CategoryMealsScreen;
